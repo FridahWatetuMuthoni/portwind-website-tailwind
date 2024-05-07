4acef82d51963163fab9b1d2f977bd631a61e588
FridahWatetu <fridahwatetumuthoni@gmail.com>
2024-05-07 09:34:59 +0300

    done

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const close = document.getElementById("close");
 const open = document.getElementById("open");
 const menu = document.getElementById("menu");
+const footer_date = document.getElementById("footer_date");
+const body = document.getElementById("body");
+const mode = document.getElementById("mode");
 
 open.addEventListener("click", handleOpen);
 close.addEventListener("click", handleClose);
 menu.addEventListener("click", handleClose);
+mode.addEventListener("click", toggleMode);
 
 function handleOpen() {
   menu.classList.remove("hidden");
@@ -13,3 +17,10 @@ function handleOpen() {
 function handleClose() {
   menu.classList.add("hidden");
 }
+
+function toggleMode() {
+  body.classList.toggle("dark");
+}
+
+const year = new Date().getFullYear();
+footer_date.innerHTML = `©${year} Portwind. Design with ♥️ by Fridah.`;
